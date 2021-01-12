6459247c6d9d6683609a048a7a5ed02fe139a3a8
tuoying96 <tuoying96@gmail.com>
2021-01-11 19:07:49 -0500

    init

---
+++ javascript/hide.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  $(".morrybayBtn").click(function(){
+    $("#morrybayPanel").slideToggle(500);
+  });
+
+  $(".haiziBtn").click(function(){
+      $(".haiziPanel").slideToggle(500);
+  });
+
+  $(".manhattanhengeBtn").click(function(){
+      $(".manhattanhengePanel").slideToggle(500);
+  });
+});
